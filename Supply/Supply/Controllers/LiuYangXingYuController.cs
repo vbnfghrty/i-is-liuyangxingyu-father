ce3ba78dc5ad0b774ffe5053c1c59a6f8eaa5a82
JiangTao-GitHub <1398713801>
2020-12-30 10:43:38 +0800

    12-30

---
+++ Supply/Supply/Controllers/LiuYangXingYuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Supply.Controllers
+{
+    public class LiuYangXingYuController : Controller
+    {
+        // GET: LiuYangXingYu
+        public ActionResult hhh()
+        {
+            return View();
+        }
+    }
+}
